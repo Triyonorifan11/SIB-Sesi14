0085ef08fd4bbdc4ecf390aa1421d0bebcc0360a
Triyonorifan11 <triyonorifan01@gmail.com>
2023-04-05 23:04:21 +0700

    latihan 1-7. latihan algoritma js

--- prima.js
+++ prima.js
@@ -5,7 +5,7 @@ function isPrima(n){
       }
     
       // mencari faktor dari n
-      for (let i = 2; i <= Math.sqrt(n); i++) {
+      for (let i = 2; i < n; i++) {
         if (n % i === 0) {
           return false;
         }
